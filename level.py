288dfd4c9fbd6e455da64fa777dce0d9e9536f50
Rita Malaquias <ana.rita.malaquias@live.com>
2022-01-26 23:49:24 +0000

    implemented background class #3

--- level.py
+++ level.py
@@ -1,1 +1,7 @@
 from pygame import *
+
+class Level():
+    def __init__(self) -> None:
+        pass
+    #meter o tile e os sprites pro background
+    #gerar um mapa de tiles
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os #helps defining path for images in case it uses different operating sy
 #from character import Character
 from player import Player
 from tile import Tile
-
+from background import Background
 #window
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -24,6 +24,33 @@ ENEMY = pygame.transform.scale(ENEMY_IMAGE, (ENEMY_WIDTH, ENEMY_HEIGHT))
 
 player = Player([100.0, 100.0], [0.01, 0.01])
 tile = Tile([10.0, 10.0])
+
+
+#TO DO: read grid from file
+grid = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+background = Background(grid)
 
 #main program
 def main():
@@ -48,12 +75,12 @@ def main():
 #methods
 def draw_window(player, enemy, tile):
     WIN.fill(BLACK)
+    for background_tile in background.tile_list:
+        WIN.blit(background_tile[0], background_tile[1]) #0 = tile_sprite, 1 = rect
     WIN.blit(player.image, (player.position[0], player.position[1])) #when you want to draw a surface onto a screen
     WIN.blit(ENEMY, (enemy.x, enemy.y))
     WIN.blit(tile.image, (tile.position[0], tile.position[1]))
     pygame.display.update() #refreshes the screen
-
-
 
 if __name__ == "__main__": #main file to run
     main()
